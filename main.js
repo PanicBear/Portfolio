f39ccf11592751123a235fcc6fbe16e080a08c2e
ClarXo <imp_rove@naver.com>
2020-11-09 18:26:40 +0900

    JS : Navbar menu click - Self

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // Make navbar transparent wehn it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
+const navbarBtn = document.querySelectorAll(".navbar__menu__item");
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
@@ -10,3 +11,29 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+navbarBtn[0].addEventListener("click", () => {
+  scrollTo(document.getElementById("home").offsetTop);
+});
+navbarBtn[1].addEventListener("click", () => {
+  scrollTo(document.getElementById("about").offsetTop);
+});
+navbarBtn[2].addEventListener("click", () => {
+  scrollTo(document.getElementById("skills").offsetTop);
+});
+navbarBtn[3].addEventListener("click", () => {
+  scrollTo(document.getElementById("work").offsetTop);
+});
+navbarBtn[4].addEventListener("click", () => {
+  scrollTo(document.getElementById("testimonials").offsetTop);
+});
+navbarBtn[5].addEventListener("click", () => {
+  scrollTo(document.getElementById("contact").offsetTop);
+});
+
+function scrollTo(x) {
+  window.scroll({
+    top: x,
+    left: 0,
+    behavior: "smooth",
+  });
+}
