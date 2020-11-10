542b4b70f2582f2ca0b3ad44edae4df40c967bcb
ClarXo <imp_rove@naver.com>
2020-11-10 18:40:21 +0900

    JS : Home contact button - Guided

--- main.js
+++ main.js
@@ -20,18 +20,16 @@ navbarMenu.addEventListener("click", (event) => {
     return;
   }
   console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
 
-const contactBtn = document.querySelector(".home__contact");
-contactBtn.addEventListener("click", (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
-  if (!link) {
-    return;
-  }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+// Handle click o0n "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
 });
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
