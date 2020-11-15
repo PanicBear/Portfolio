b95893aefe84be392b45db4cc7b8ffa37db1cc96
ClarXo <imp_rove@naver.com>
2020-11-15 19:34:05 +0900

    JS : Remove active from nav - Self

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Make navbar transparent wehn it is on the top
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -11,19 +11,26 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// Handle scrolling when tapping on the navbar menu
+// Handle scrolling when tapping on the navbar menu & make active onclick
 const navbarMenu = document.querySelector(".navbar__menu");
+const navbarMenuItems = document.querySelectorAll(".navbar__menu__item");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (!link) {
     return;
   }
-  console.log(event.target.dataset.link);
   scrollIntoView(link);
+  Array.from(navbarMenuItems).forEach((e) => {
+    if (e === target) {
+      e.classList.add("active");
+    } else {
+      e.classList.remove("active");
+    }
+  });
 });
 
-// Handle click o0n "contact me" button on home
+// Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
@@ -53,6 +60,7 @@ arrowUp.addEventListener("click", () => {
 
 // Projects
 const workBtnContainer = document.querySelector(".work__categories");
+const categoryBtns = document.querySelectorAll(".category__btn");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
 workBtnContainer.addEventListener("click", (e) => {
@@ -60,10 +68,16 @@ workBtnContainer.addEventListener("click", (e) => {
   if (!filter) {
     return;
   }
+  Array.from(categoryBtns).forEach((categoryBtn) => {
+    if (e.target === categoryBtn) {
+      categoryBtn.classList.add("active");
+    } else {
+      categoryBtn.classList.remove("active");
+    }
+  });
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
-      console.log(project.dataset.type);
       if (filter === "*" || filter === project.dataset.type) {
         project.classList.remove("invisible");
       } else {
