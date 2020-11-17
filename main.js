86edbeb4a9412d0cd0cf4b67816b912fe6dbd6fe
ClarXo <imp_rove@naver.com>
2020-11-17 20:00:09 +0900

    JS : Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -11,31 +11,29 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// Navbar toggle btn
-const navbarToggler = document.querySelector(".navbar__toggle-btn");
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarToggler.addEventListener("click", () => {
-  if (!navbarMenu.classList.contains("responsive")) {
-    navbarMenu.classList.add("responsive");
-  } else {
-    navbarMenu.classList.remove("responsive");
-  }
-});
-
-window.addEventListener("resize", () => {
-  if (window.matchMedia("(max-width: 768px)").matches === false) {
-    navbarMenu.classList.remove("responsive");
-  }
-});
-
 // Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (!link) {
     return;
   }
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
+// Personal modification
+window.addEventListener("resize", () => {
+  if (window.matchMedia("(max-width: 768px)").matches === false) {
+    navbarMenu.classList.remove("open");
+  }
 });
 
 // Handle click on "contact me" button on home
